41ec9501d56fa0cd670d11e370b14d16180a479c
TruongQuocHuy-fr <truongquochuy6767@gmail.com>
2024-09-26 20:30:08 +0700

    first

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,13 +9,12 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDicxgSWEfcnw2sHCp7eJyCXm9mbQeF1JI",
-  authDomain: "fir-login-7b798.firebaseapp.com",
-  projectId: "fir-login-7b798",
-  storageBucket: "fir-login-7b798.appspot.com",
-  messagingSenderId: "7484503944",
-  appId: "1:7484503944:web:f6db15222a5efc2e7dac3d",
-  measurementId: "G-2WCVPCLRZT"
+  apiKey: "AIzaSyARVgNSIpLF3vzE5LWOfboSUfieanHRzhk",
+  authDomain: "connectdata-ce589.firebaseapp.com",
+  projectId: "connectdata-ce589",
+  storageBucket: "connectdata-ce589.appspot.com",
+  messagingSenderId: "1000178099654",
+  appId: "1:1000178099654:web:87c52450d88b6769f6fec7"
 };
 
 // Initialize Firebase
